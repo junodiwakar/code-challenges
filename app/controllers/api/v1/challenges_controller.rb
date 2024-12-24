a8faa8e31a63ca6ecc75350d7e8b220eb90ba72c
junodiwakar <junodiwakar@gmail.com>
2024-12-24 11:52:51 +0530

    Created RESTapi for challenge model

--- app/controllers/api/v1/challenges_controller.rb
+++ app/controllers/api/v1/challenges_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
     class ChallengesController < ApplicationController
+      before_action :set_challenge, only: %i[show update destroy]
 
       def index
         @challenges = Challenge.all
@@ -8,17 +9,47 @@ module Api
       end
 
       def create
+        @challenge = Challenge.new(challenge_params)
+        if @challenge.save
+          render json:{message: "Challenge created successfully", data: @challenge}
+        else
+          render json:{message: "Failed to create challenge", data: @challenge.errors}
+        end
       end
 
       def show
+        if @challenge
+          render json:{ message: "Challenge found", data: @challenge}
+        else
+          render json:{message: "Challenge not found", data: @challenge.errors}
+        end
       end
 
       def update
+        if @challenge.update(challenge_params)
+          render json: {message: "Challenge updated successfully", data: @challenge}
+        else
+          render json: {message: "Challenge not updated successfully", data: @challenge.errors}
+        end
       end
 
       def destroy
+        if @challenge.destroy
+          render json: {message: "Challenge deleted successfully", data: @challenge}
+        else
+          render json: {data: @challenge.errors}
+        end
       end
 
+      private
+
+      def set_challenge
+        @challenge = Challenge.find(params[:id])
+      end
+
+      def challenge_params
+        @challenge = params.require(:challenge).permit(:title, :description, :start_date, :end_date)
+      end
     end
   end
 end
