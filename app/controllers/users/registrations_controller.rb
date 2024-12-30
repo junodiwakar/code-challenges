1edf906f13bdca446df6be258d9793d6be692f0a
junodiwakar <junodiwakar@gmail.com>
2024-12-30 14:55:23 +0530

    overide the Destroy method for user

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,6 +4,19 @@ class Users::RegistrationsController < Devise::RegistrationsController
   include RackSessionFix
   respond_to :json
 
+  def destroy
+    resource = resource_class.find_by(id: current_user.id)
+    if resource&.destroy
+      render json: {
+        status: { code: 200, message: "Account deleted successfully." }
+      }, status: :ok
+    else
+      render json: {
+        status: { code: 422, message: "Account could not be deleted. #{resource.errors.full_messages.to_sentence}" }
+      }, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def respond_with(resource, _opts = {})
