d20411aa3e19a7eb3bfe4300f2aaea061938d283
junodiwakar <junodiwakar@gmail.com>
2024-12-31 12:16:39 +0530

    Mapped user and challenges, created require admin method for challenges controller

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,3 +1,4 @@
 class Challenge < ApplicationRecord
+  belongs_to :user
   validates :title, :description, :start_date, :end_date, presence: true
 end
--- db/migrate/20241230062837_add_index_to_users_jti.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToUsersJti < ActiveRecord::Migration[7.1]
-  def change
-    add_index :users, :jti, unique: true
-  end
-end
